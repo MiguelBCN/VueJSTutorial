f92f9f12ae5a66d595b0d1aea0b59047c8c4a4ed
Miguel Huayllas <huayllasmiguel@gmail.com>
2022-03-31 00:25:00 +0200

    Inicio de tutorial en github

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Helo vuie");
+
+const app = Vue.createApp({
+    // This contatins data, functions, template to render
+    //template:'<h2>I am template </h2>'
+    data(){
+        return{
+            title:'The empire strikes back',
+            author:"George martin",
+            year: "2020",
+            showBooks:true
+        }
+    },
+    methods:{
+        changeTitle(title){
+            console.log("You clcik")
+            this.title=title// Remember to put "this" to refer the data
+        },
+        showBooks2(){
+            this.showBooks = !this.showBooks
+        }
+
+    }
+})
+
+app.mount('#app')
