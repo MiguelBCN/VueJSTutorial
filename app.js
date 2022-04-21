67d718eac1d65f2495420213744b2b25897c0a0a
miguelBCN <migue.shar.17@gmail.com>
2022-04-21 18:15:38 +0200

    bootstrap + vue rouer + vue normal

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-console.log("Helo vuie");
 
-const app = Vue.createApp({
-    // This contatins data, functions, template to render
-    //template:'<h2>I am template </h2>'
-    data(){
-        return{
-            title:'The empire strikes back',
-            author:"George martin",
-            year: "2020",
-            showBooks:true
-        }
-    },
-    methods:{
-        changeTitle(title){
-            console.log("You clcik")
-            this.title=title// Remember to put "this" to refer the data
-        },
-        showBooks2(){
-            this.showBooks = !this.showBooks
-        }
+console.log("hello")
 
-    }
+// 1. Define route components.
+// These can be imported from other files
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+const Contat = { template: '<div>Contact</div>'}
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  {path: '/contact', component :Contat},
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes, // short for `routes: routes`
 })
 
+// 5. Create and mount the root instance.
+const app = Vue.createApp({
+    data(){
+        return{
+            nota:"proba"
+        }
+    },
+})
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
 app.mount('#app')
+
+// Now the app has started!S
